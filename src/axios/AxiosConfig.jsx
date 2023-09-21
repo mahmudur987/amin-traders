527099a907118065fe4df80e30d5162ad092b767
mahmudur987 <108387570+mahmudur987@users.noreply.github.com>
2023-09-21 06:38:24 +0600

    Update AxiosConfig.jsx

--- src/axios/AxiosConfig.jsx
+++ src/axios/AxiosConfig.jsx
@@ -1,10 +1,10 @@
 import Axios from "axios";
-const AxiosBaseURL = Axios.create({
-  baseURL: "https://amin-traders-server.vercel.app/",
-});
-
 // const AxiosBaseURL = Axios.create({
-//   baseURL: "http://localhost:8000/",
+//   baseURL: "https://amin-traders-server.vercel.app/",
 // });
+
+const AxiosBaseURL = Axios.create({
+  baseURL: "http://localhost:8000/",
+});
 
 export default AxiosBaseURL;
